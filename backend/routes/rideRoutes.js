f1cd203b5159e8ec899a0fb9b9e966f5e371cd5a
mdouaour <yacine23i@hotmail.com>
2025-10-24 17:27:00 +0100

    feat(backend): Restrict updateRideStatus route to drivers and admins

--- backend/routes/rideRoutes.js
+++ backend/routes/rideRoutes.js
@@ -12,7 +12,7 @@ import { protect, driver, admin } from "../middlewares/authMiddleware.js";
 router.route("/").post(protect, createRide);
 router.route("/myrides").get(protect, getMyRides);
 router.route("/:id").get(protect, getRideById);
-router.route("/:id/status").put(protect, updateRideStatus);
+router.route("/:id/status").put(protect, driver, admin, updateRideStatus);
 router.route("/:id/accept").put(protect, driver, acceptRide);
 
 export default router;
